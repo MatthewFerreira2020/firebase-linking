e9db51f9813caffc514bc33aca86baf1dcd774bb
MatthewFerreira2020 <38076237+MatthewFerreira2020@users.noreply.github.com>
2020-01-27 21:50:49 -0500

    Add files via upload

---
+++ linking firebase/bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  linking firebase
+//
+//  Created by Axesniper on 1/27/20.
+//  Copyright © 2020 Matt. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "Firebase.h"
+
+#endif /* bridge_h */
